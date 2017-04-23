2da74a0c9a2b205e429cfd03eda8edbf89690cfd
aadityatamrakar <aaditya.span@gmail.com>
2017-04-23 19:04:22 +0530

    asd

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 Route::group(['prefix' => '/v1', 'namespace' => 'Api\V1', 'as' => 'api.'], function () {
-
         Route::resource('dealers', 'DealersController');
-
+        Route::resource('calls', 'CallsController');
 });
